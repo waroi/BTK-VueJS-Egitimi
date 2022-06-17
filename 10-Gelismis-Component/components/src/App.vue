3220cbc17655aaf92506a4c1376c9472accb5dae
Varol Maksutoğlu <waroi@pisilinux.org>
2022-06-18 02:37:59 +0300

    advanced-component

---
+++ 10-Gelismis-Component/components/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Header>
+      <h1>Bu bir deneme başlığı</h1>
+    </Header>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 10-Gelismis-Component/components/src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <slot></slot>
+    <hr>
+    <input type="text" v-model="name">
+    <input type="text" v-model="surname">
+    <p>{{nameSurname}}</p>
+    <hr>
+    <input type="number" v-model="number1"> <br>
+    <input type="number" v-model="number2"> <br>
+    <input type="number" v-model="total">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name:'',
+      surname:'',
+      number1:0,
+      number2:0,
+      total:0
+    }
+  },
+  watch: {
+    number1(newValue, oldValue) {
+      console.log('Sayılar',oldValue,newValue);
+      this.total = parseInt(this.number1) + parseInt(this.number2)
+    },
+    number2(newValue, oldValue) {
+      console.log('Sayılar',oldValue,newValue);
+      this.total = parseInt(this.number1) + parseInt(this.number2)
+    }
+  },
+  computed: {
+    nameSurname(){
+      return this.name + ' ' + this.surname
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
